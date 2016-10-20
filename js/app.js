558711c960a7cbcd8207cc1fa5e7ee61a14fed31
Ariana Clark <ariana.clark@gmail.com>
2016-10-20 14:30:33 -0600

    numbers and arithmetic operators functioning

--- js/app.js
+++ js/app.js
@@ -4,39 +4,42 @@ $(document).ready(function () {
 
 // **** Variables ****
   var numericButtons = $('span').not('.operator');
-  var operatorButtons = $('span').hasClass('operator')
+  var operatorButtons = $('.operator');
   var screenText = $('#screen').text();
   var calcScreen = $('#screen');
-  var target = $(this);
-
+  var target;
+  console.log(operatorButtons);
 // **** Event Listeners ****
 $(numericButtons).on('click',textToScreen);
-
+$(operatorButtons).on('click',operatorSwitch);
 // **** Functions ****
 
 function textToScreen() {
-  console.log(target);
+  console.log(operatorButtons);
+  console.log(this);
   var concatText = calcScreen.text() + $(this).text();
   calcScreen.html(concatText);
 }
-// function operatorSwitch() {
-//   switch (target.text()) {
-//     case '+':
-//       textToScreen();
-//       break;
-//     case '-':
-//       textToScreen();
-//       break;
-//     case 'x':
-//       textToScreen();
-//       break;
-//     case '÷':
-//       textToScreen();
-//       break;
-//     default:
-//       break;
-//   }
-// }
+
+// refactor operatorSwitch later
+function operatorSwitch() {
+  switch ($(this).text()) {
+    case '+':
+      calcScreen.html(calcScreen.text() + $(this).text());
+      break;
+    case '-':
+      calcScreen.html(calcScreen.text() + $(this).text());
+      break;
+    case 'x':
+      calcScreen.html(calcScreen.text() + $(this).text());
+      break;
+    case '÷':
+      calcScreen.html(calcScreen.text() + $(this).text());
+      break;
+    default:
+      break;
+  }
+}
 
 
 
